e4fb10cdf7e7bc1cc541b3d46ff9cd29e7446dea
Jon <lundbyjon@hotmail.com>
2023-04-20 22:24:38 +0200

    form and submit added. when submitting the console shows the submitted object

--- script.js
+++ script.js
@@ -3,5 +3,42 @@
 window.addEventListener("load", startApp);
 
 function startApp() {
-    console.log("app is up and running...")
+  console.log("app is up and running...");
+    document.querySelector("#sign-up").addEventListener("submit", clickSubmit);
+    
+    document.querySelector("#terms").addEventListener("click", clickAccept);
+}
+
+function clickAccept(event) {
+    console.log(event.target.checked);
+    console.log("accept changed");
+    if (event.target.checked === true) {
+        document.querySelector("#btn-submit").disabled = false;        
+    } else {
+        document.querySelector("#btn-submit").disabled = true;
+    }
+
+        // document.querySelector("#btn-submit").diabled = true;
+}
+
+function clickSubmit(event) {
+  console.log("target:");
+  console.log(event.target);
+
+  console.log("currentTarget:");
+  console.log(event.currentTarget);
+
+  event.preventDefault();
+
+  const signup = {
+    fullname: document.querySelector("form").elements.namedItem("fullname").value,
+    email: document.querySelector("form").elements.namedItem("email").value,
+    username: document.querySelector("form").elements.namedItem("username").value,
+    password: document.querySelector("form").elements.namedItem("password").value,
+    payment: document.querySelector("form").elements.namedItem("payment").value,
+    payevery: document.querySelector("form").elements.namedItem("pay-every").value,
+    newsletter: document.querySelector("form").elements.namedItem("newsletter").value,
+  };
+
+  console.log(signup);
 }
